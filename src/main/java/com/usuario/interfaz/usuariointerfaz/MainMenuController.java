3a60cf5f04efac3aae9fe810511d809b850788bf
paola <pvzambrano.24@est.ucab.edu.ve>
2025-01-03 19:07:33 -0400

    Agregar popup de salir

--- src/main/java/com/usuario/interfaz/usuariointerfaz/MainMenuController.java
+++ src/main/java/com/usuario/interfaz/usuariointerfaz/MainMenuController.java
@@ -1,7 +1,13 @@
 package com.usuario.interfaz.usuariointerfaz;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainMenuController {
     public Button crearUsuarioButton;
@@ -26,7 +32,12 @@ public class MainMenuController {
         System.out.println("Eliminar usuario click");
     }
 
-    public void salirUsuario(MouseEvent mouseEvent) {
-        System.out.println("Salir click");
+    public void salirUsuario(MouseEvent mouseEvent) throws IOException {
+        final Stage dialog = new Stage();
+        dialog.initModality(Modality.APPLICATION_MODAL);
+        FXMLLoader fxmlLoader = new FXMLLoader(MainMenuApplication.class.getResource("popup-cerrar.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 420, 206);
+        dialog.setScene(scene);
+        dialog.show();
     }
 }
