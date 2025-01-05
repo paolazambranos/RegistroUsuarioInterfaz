0227b4548ea31a477a69af8069741eb23a89f87b
paola <pvzambrano.24@est.ucab.edu.ve>
2025-01-05 17:42:55 -0400

    Crear ventana para que muestre lista de usuarios Imprima cuando el usuario no es seleccionado

--- src/main/java/com/usuario/interfaz/usuariointerfaz/ListaUsuariosController.java
+++ src/main/java/com/usuario/interfaz/usuariointerfaz/ListaUsuariosController.java
@@ -6,8 +6,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -18,13 +20,25 @@ public class ListaUsuariosController {
 
     public Button verDetallesUsuarioButton;
     public Button volverMenuButton;
-    public ListView listaUsuarios;
+    public ListView<String> listaUsuarios;
+    public Pane detallesPanel;
+    public Button volverListaButton;
+    public Pane listaPanel;
+    public Label aliasValor;
+    public Label correoValor;
+    public Label puntosTotalesValor;
+    public Label horasJugadasValor;
+    public Label palabrasTotalesValor;
+    public Label mensajeError;
     private Stage stage;
     private Scene scene;
 
+    private String usuarioSeleccionado;
+    private List<Usuario> usuariosGuardados;
+
     public void initialize() {
         Lector lector = new Lector();
-        List<Usuario> usuariosGuardados = lector.obtenerUsuariosGuardados();
+        usuariosGuardados = lector.obtenerUsuariosGuardados();
         for(Usuario usuario: usuariosGuardados){
             listaUsuarios.getItems().add(usuario.getAlias()+" - "+usuario.getCorreo());
         }
@@ -39,6 +53,53 @@ public class ListaUsuariosController {
         stage.show();
     }
 
-    public void verDetallesUsuario(MouseEvent mouseEvent) {
+    public void verDetallesUsuario(MouseEvent mouseEvent) throws IOException {
+        //Limpiar interfaz
+        mensajeError.setVisible(false);
+
+        //obteniendo usuario seleccionado
+        usuarioSeleccionado = listaUsuarios.getSelectionModel().getSelectedItem();
+        if(usuarioSeleccionado != null && !usuarioSeleccionado.isEmpty()) {
+            Usuario usuarioEncontrado = getUsuarioEnUsuariosGuardados();
+
+            //asignar a cada label los datos de usuarioEncontrado
+            if(usuarioEncontrado != null) {
+                aliasValor.setText(usuarioEncontrado.getAlias());
+                correoValor.setText(usuarioEncontrado.getCorreo());
+                puntosTotalesValor.setText(String.valueOf(usuarioEncontrado.getPuntosTotales()));
+                horasJugadasValor.setText(String.valueOf(usuarioEncontrado.getHorasJugadas()));
+                palabrasTotalesValor.setText(String.valueOf(usuarioEncontrado.getCantidadDePalabras()));
+                listaPanel.setVisible(false);
+                detallesPanel.setVisible(true);
+            }else{
+                mensajeError.setText("Usuario no encontrado");
+                mensajeError.setVisible(true);
+            }
+        }
+        else{
+
+            mensajeError.setText("Usuario no seleccionado, por favor seleccione un usuario\"");
+            mensajeError.setVisible(true);
+        }
+
+
+
+    }
+
+    private Usuario getUsuarioEnUsuariosGuardados() {
+        String[] dataUsuario = usuarioSeleccionado.split(" - ");
+        Usuario usuarioEncontrado = null;
+        for (Usuario usuario : usuariosGuardados) {
+            if (usuario.getCorreo().equalsIgnoreCase(dataUsuario[1])) {
+                usuarioEncontrado = usuario;
+                break;
+            }
+        }
+        return usuarioEncontrado;
+    }
+
+    public void volverLista(MouseEvent mouseEvent) {
+        listaPanel.setVisible(true);
+        detallesPanel.setVisible(false);
     }
 }
